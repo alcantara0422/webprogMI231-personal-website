53c3eb91f52f671978abc3725f736c8f15f0ed3d
alcantara0422 <mmalcantara@student.apc.edu.ph>
2024-12-16 08:15:30 +0000

    ADDED

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,4 +1,11 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("p");
+
+  document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
   
+
+}
